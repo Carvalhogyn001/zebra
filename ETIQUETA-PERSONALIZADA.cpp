0a8697b1adb1eeee6fdbef8f15ba96d18b2de80a
Christian Alves <ca.oaky@gmail.com>
2017-04-27 22:44:57 -0300

    Update ETIQUETA-PERSONALIZADA.cpp

--- ETIQUETA-PERSONALIZADA.cpp
+++ ETIQUETA-PERSONALIZADA.cpp
@@ -10,6 +10,10 @@
 #include <stdio.h>
 #include <conio.h> 
 
+/******************************************
+***        CHRISTIAN ALVES               ***
+***       ca.oaky@gmail.com              ***
+********************************************/
 
 using namespace std;
 
